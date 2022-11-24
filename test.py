b09bd2894ce4306d7c72ef2498baa0e975e6f5c0
jasmine <751925352@qq.com>
2022-11-24 09:49:49 +0800

    无语，本地python环境gg了”

--- test.py
+++ test.py
@@ -19,14 +19,19 @@ with open("./data/res.csv") as f:
         p_list.append(tmp)
         line = f.readline()
 p_list = np.asarray(p_list)
-# print(p_list[:, -1])
+
 # 计算明文簇相关信息
 p_id_set = set(p_list[:, -2])
-p_id_list = list(p_id_set)
-p_id_list.sort()
-print("明文计算簇分类："+str(p_id_list))
+print("明文计算簇分类："+str(p_id_set))
 
-
+p_id_dict = dict()
+for item in p_list[:, -2]:
+    if item in p_id_dict:
+        p_id_dict[item] = p_id_dict[item]+1
+    else:
+        p_id_dict[item] = 0
+    
+print(p_id_dict)
 
 # 计算密文簇相关信息
 all_id_set = set()
@@ -39,6 +44,23 @@ noise_id_set = all_id_set - c_id_set
 c_id_list = list(c_id_set)
 noise_id_list = list(noise_id_set)
 
-c_id_list.sort()
 print("密文聚类簇分类："+str(c_id_list))
 print("噪声簇分类："+str(noise_id_list))
+
+c_id_dict_tmp = dict()
+for item in p_list[:, -1]:
+    if item in noise_id_set:
+        continue
+    if item in c_id_dict_tmp:
+        c_id_dict_tmp[item] = c_id_dict_tmp[item] + 1
+    else:
+        c_id_dict_tmp[item] = 0
+        
+sort_tmp = sorted(c_id_dict_tmp)
+key = 1
+c_id_dict = dict()
+for item in sort_tmp:
+    c_id_dict[key] = c_id_dict_tmp[item]
+    key = key + 1
+
+print(c_id_dict)
