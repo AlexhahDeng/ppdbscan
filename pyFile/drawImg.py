fc5bbda4ff9d16990da20c9062599c872d19af1d
alexhahdeng <751925352@qq.com>
2022-12-14 17:03:14 +0800

    完成shuffle工作

--- pyFile/drawImg.py
+++ pyFile/drawImg.py
@@ -4,6 +4,16 @@ import numpy as np
 import sys
 from time import strftime, localtime
 
+# 预定义颜色
+colors = np.array(
+    ['g', 'r', 'c', 'm', 'y', 'k', 
+     '#2ecc71', '#ff7f50', '#2ed573', 
+     '#f0932b', '#82589f', '#7efff5',
+     '#ea5514', '#6a8d52','#12507b', '#e94829',
+     '#ddbb99', '#2c4f54', '#c3d825', '#cc7eb1', 
+     '#c9171e', '#ea5506'])
+
+# 提取噪声点
 def extraNoise(datalist):
     dataset = set()
     repeatset = set()
@@ -12,13 +22,14 @@ def extraNoise(datalist):
             repeatset.add(data[-1])
             continue
         dataset.add(data[-1])
-    return dataset-repeatset # 就是只出现一次的noise
+    return dataset-repeatset 
 
-setname = sys.argv[1]
 # 获取文件名
+setname = sys.argv[1]
 prefix = strftime("%m-%d", localtime())
 filename = "/home/dd/Ubu20/ppdbscan/res/" +prefix+ "-" + setname+".csv"
 
+# 读取数据
 csv_reader = csv.reader(open(filename))
 datalist = []
 for line in csv_reader:
@@ -26,19 +37,15 @@ for line in csv_reader:
     for item in line:
         cur.append(int(item))
     datalist.append(cur)
-
-colors = np.array(
-    ['g', 'r', 'c', 'm', 'y', 'k', 'w', '#2ecc71', '#ff7f50', '#2ed573', '#f0932b', '#82589f', '#7efff5',
-     '#ea5514', '#6a8d52',
-     '#12507b', '#e94829',
-     '#ddbb99', '#2c4f54', '#c3d825', '#cc7eb1', '#c9171e', '#ea5506'])
 datanp = np.asarray(datalist)
 
+# 查找噪声点
 noiseset = extraNoise(datalist)
 print("noist set is ", str(noiseset))
 
-# 无重复的簇标记
+# 无重复的簇
 label = set(datanp[:, -1])
+
 # 创建映射
 label2color = {}
 count = 0
@@ -48,11 +55,11 @@ for idx, val in enumerate(label):
         continue
     label2color[val] = colors[count % len(colors)]
     count+= 1
-
 print("num of cluster ", str(len(label) - len(noiseset)))
 x = datanp[:, 0]
 y = datanp[:, 1]
 
+#开始绘图
 for i in range(len(datanp)):
     plt.scatter(x[i], y[i], s=10, c=label2color[datanp[i, -1]])
 
